01a0e5b2da29f7a8f3f5b94be762fc781d9f5f05
Davydov Albert <103847309+VerS7@users.noreply.github.com>
2023-07-31 00:11:08 +0300

    some fixes

--- src/sharing.py
+++ src/sharing.py
@@ -1,4 +1,9 @@
 from git import Repo
+import os
+import shutil
+
+
+DEFAULT_LOCAL = "../local"
 
 
 class GitWorks:
@@ -6,18 +11,22 @@ class GitWorks:
         self.__localrepo = self.__create_repo(path, remote_url)
 
     def __create_repo(self, path, url):
+        if not os.path.exists(DEFAULT_LOCAL):
+            os.makedirs(DEFAULT_LOCAL)
         try:
             local_repo = Repo(path)
         except:
+            shutil.rmtree(DEFAULT_LOCAL)
+            os.makedirs(DEFAULT_LOCAL)
             local_repo = Repo.clone_from(url, path)
         return local_repo
 
-    def local_add(self, file, comment=None):
-        self.__localrepo.git.add(file)
+    def local_add(self, filename, comment=None):
+        self.__localrepo.git.add(filename)
         self.__localrepo.git.commit("-m", comment if comment is not None else "Small changes")
 
     def local_remote_push(self):
         self.__localrepo.remotes.origin.push()
 
-    def local_update(self):
+    def local_remote_update(self):
         self.__localrepo.remotes.origin.pull()
