439b06c86597e5d0c9b400d96b1b96eaf4b89886
Davydov Albert <103847309+VerS7@users.noreply.github.com>
2023-08-13 23:45:20 +0300

    small changes and docstrings

--- src/sharing.py
+++ src/sharing.py
@@ -7,6 +7,7 @@ DEFAULT_LOCAL = "../local"
 
 
 class GitWorks:
+    """Working with GIT system"""
     def __init__(self, path, remote_url):
         self.__localrepo = self.__create_repo(path, remote_url)
 
@@ -18,18 +19,39 @@ class GitWorks:
         except:
             shutil.rmtree(DEFAULT_LOCAL)
             os.makedirs(DEFAULT_LOCAL)
-            local_repo = Repo.clone_from(url, path)
+            try:
+                local_repo = Repo.clone_from(url, path)
+            except Exception as e:
+                raise e
         return local_repo
 
+    def get_commits(self, filepath):
+        """
+        :param filepath: path to file from local repository
+        :return: last commit datetime and comment
+        """
+        commits = list(self.__localrepo.iter_commits(paths=filepath))
+        return commits[::-1][0].committed_datetime, commits[::-1][0].message
+
     def local_add(self, filename, comment=None):
+        """
+        :param filename: filename with extension e.g. file.xml
+        :param comment: commit comment. "Small changes" if None
+        """
         try:
             self.__localrepo.git.add(filename)
         except Exception as e:
-            print(e)
+            raise e
         self.__localrepo.git.commit("-m", comment if comment is not None else "Small changes")
 
     def local_remote_push(self):
-        self.__localrepo.remotes.origin.push()
+        try:
+            self.__localrepo.remotes.origin.push()
+        except Exception as e:
+            raise e
 
     def local_remote_update(self):
-        self.__localrepo.remotes.origin.pull()
+        try:
+            self.__localrepo.remotes.origin.pull()
+        except Exception as e:
+            raise e
